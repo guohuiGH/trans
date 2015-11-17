94f9954ed3efd14c1645cc1cd818a916edd98b4a
guohuiGH <guohui2011915@gmail.com>
2015-11-18 00:52:04 +0800

    ma,mi,av

--- script/average_times.py
+++ script/average_times.py
@@ -48,6 +48,33 @@ def read_data(file_name):
     my_file.close()
     return name_day_hour
 
+def get_average_week(name_day_hour):
+    name_week_hour = dict()
+    day_hours = name_day_hour.keys()
+    for line in day_hours:
+        line_list = line.split(',')
+        line_list[1] = str(int(line_list[1])%7)
+        name = ','.join(line_list)
+        
+        temp_list = list()
+        if name in name_week_hour.keys():
+            temp_list = name_week_hour[name]
+        temp_list.append(name_day_hour[line])
+        name_week_hour[name] = temp_list
+    return name_week_hour
+
+def get_max_min_aver(name_week_hour):
+    name_week_hour_min = dict(); name_week_hour_max = dict(); name_week_hour_aver = dict();
+    week_hours = name_week_hour.keys()
+    for line in week_hours:
+        temp_list = name_week_hour[line]
+        name_week_hour_max[line] = max(temp_list)
+        name_week_hour_min[line] = min(temp_list)
+        name_week_hour_aver[line] = sum(temp_list)/len(temp_list)
+    return name_week_hour_max, name_week_hour_min, name_week_hour_aver
+
+
+
 def write_file(test_name_day_hour,file_name):
     global xianlu
     my_file = open(file_name, 'w+')
@@ -57,7 +84,7 @@ def write_file(test_name_day_hour,file_name):
         nums = test_name_day_hour[line]
         line_list = line.split(',')
         line_list[0] = xianlu + line_list[0]
-        line_list[1] = '2015010' + str(int(line_list[1]) - 146)
+        line_list[1] = '2015010' + str(int(line_list[1]) +1)
         hour = line_list[2]
         if hour< '06'or hour > '21':
             continue
@@ -70,7 +97,15 @@ def write_file(test_name_day_hour,file_name):
 
 def main():
     train_file = '../tmp/gd_train'
-    #train_name_day_hour = read_data(train_file)
+    train_name_day_hour = read_data(train_file)
+    train_name_week_hour = get_average_week(train_name_day_hour)
+    (maxValue, minValue, averValue) = get_max_min_aver(train_name_week_hour)
+    predict_file = '../tmp/predict_av.txt'
+    write_file(averValue, predict_file)
+    predict_file = '../tmp/predict_ma.txt'
+    write_file(maxValue, predict_file)
+    predict_file = '../tmp/predict_mi.txt'
+    write_file(minValue, predict_file)
     test_file = '../tmp/gd_test'
     test_name_day_hour = read_data(test_file)
     predict_file = '../tmp/predict'
